834a45a2282546c00243a32eeb67f340150c164b
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2021-05-01 22:43:17 +0700

    add websocket (beta)

---
+++ websocket.js
@@ -0,0 +1,52 @@
+const { Inflate } = require('pako');
+
+function camelCase(string) {
+    string = string.split('_');
+    return string[0].toLowerCase() + (string.length > 1 ? string.slice(1).map(x => `${x[0].toUpperCase()}${x.slice(1).toLowerCase()}`).join('') : '');
+}
+
+function evaluate(msg, flag) {
+    if (!flag || typeof flag !== 'object') flag = {};
+    if (!flag.binary) return JSON.parse(msg);
+    const inflator = new Inflate();
+    inflator.push(msg);
+    if (inflator.err) throw new Error('An error occurred while decompressing data');
+    return JSON.parse(inflator.toString());
+}
+
+module.exports = (bot, message, flag) => {
+    const msg = evaluate(message, flag);
+    bot.emit('debug', `Received a raw gateway event with the OP code of ${msg.op}.`);
+    
+    switch (msg.t) {
+        case 'READY':
+            if (!msg.d.user.bot) process.exit(1); // no selfbots allowed uwu
+            bot.sessionID = msg.d.session_id;
+            bot.user = msg.d.user;
+            bot.emit('ready');
+            break;
+        default:
+            if (msg.t && msg.d)
+                bot.emit(camelCase(msg.t), msg.d);
+    }
+    
+    switch (msg.op) {
+        case 7: // reconnect
+            return bot.socket.send(JSON.stringify({
+                op: 6,
+                d: {
+                    session_id: bot.sessionID,
+                    token: bot.token,
+                    seq: bot.seqNum
+                }
+            }));
+        case 10: // heartbeat
+            if (bot.hb) clearInterval(bot.hb);
+            bot.hb = setInterval(() => {
+                return bot.socket.send(JSON.stringify({
+                    op: 1,
+                    d: 1
+                }));
+            }, msg.d.heartbeat_interval);
+    }
+}
