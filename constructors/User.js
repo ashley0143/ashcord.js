02c51e9751335d829882ce6f8371a23455d2137f
Voltrex <62040526+VoltrexMaster@users.noreply.github.com>
2021-05-03 17:23:32 +0430

    Fix User class
    Fixed `User` class with detail changes, more convenient checks and correct identation.

--- constructors/User.js
+++ constructors/User.js
@@ -1,50 +1,56 @@
 const { snowflakeDate } = require('../util');
 
 class User {
-    constructor(data) {
-        this.verified = data.verified || false;
-        this.name = data.username;
-        this.mfaEnabled = data.mfa_enabled || false;
-        this.id = data.id;
-        this.flags = data.flags || 0;
-        this.email = data.email || null;
-        this.discriminator = data.discriminator || '0000';
-        this.bot = data.bot || false;
-        this.avatar = data.avatar || null;
-    }
+  constructor(data) {
+    this.verified = !!data.verified;
+    this.username = data.username;
+    this.mfaEnabled = !!data.mfa_enabled;
+    this.id = data.id;
+    this.flags = data.flags || 0;
+    this.email = data.email || null;
+    this.discriminator = data.discriminator || '0000';
+    this.bot = !!data.bot;
+    this.avatar = data.avatar || null;
+  }
     
-    avatarURL({ format, size }) {
-        if (this.avatar) {
-            if (format) format = format.toLowerCase().replace(/\./g, '');
-            if (format && !['gif', 'png', 'jpeg', 'jpg', 'webp'].includes(format)) throw new TypeError(`Invalid format: ${format}`);
-            if (format === 'gif' && !this.avatarAnimated) format = 'png';
-    
-            size = Number(size) || 4096;
-            if (size & (size - 1) || (size < 16 || size > 4096)) throw new TypeError(`The size must be a power of two between 16 and 4096.`);
-            return `https://cdn.discordapp.com/avatars/${this.id}/${this.avatar}.${format || (this.avatarAnimated ? 'gif' : 'png')}?size=${size}`;
-        }
-        return `https://cdn.discordapp.com/embed/avatars/${Number(this.discriminator) % 5}.png`;
+  avatarURL({ format, size }) {
+    if (this.avatar) {
+      if (typeof format === 'string') {
+        format = format.toLowerCase().replace(/\./g, '');
+        if (!['gif', 'png', 'jpeg', 'jpg', 'webp'].includes(format)) throw new TypeError(`Invalid format: ${format}`);
+        if (format === 'gif' && !this.avatarAnimated) format = 'png';
+      } else format = 'png';
+
+      size = Number.isInteger(size) ? size : 4096;
+
+      if (size & (size - 1) || (size < 16 || size > 4096)) throw new TypeError('The size must be a power of two between 16 and 4096.');
+
+      return `https://cdn.discordapp.com/avatars/${this.id}/${this.avatar}.${format}?size=${size}`;
     }
+
+    return `https://cdn.discordapp.com/embed/avatars/${Number(this.discriminator) % 5}.png`;
+  }
     
-    get avatarAnimated() {
-        return this.avatar.startsWith('a_');
-    }
+  get avatarAnimated() {
+    return this.avatar.startsWith('a_');
+  }
     
-    get createdAt() {
-        return snowflakeDate(this.id);
-    }
+  get createdAt() {
+    return snowflakeDate(this.id);
+  }
 }
 
 class ClientUser extends User {
-    constructor(bot, data) {
-        super(data);
+  constructor(bot, data) {
+    super(data);
         
-        this.changeName = async (username) => {
-            if (!username || this.name === username) return;
-            await bot.request('PATCH', '/users/@me', { username });
-            this.name = username;
-        };
-    }
+    this.changeName = async (username) => {
+      if (typeof username !== 'string' || this.username === username) return this;
+      await bot.request('PATCH', '/users/@me', { username });
+      this.username = username;
+      return this;
+    };
+  }
 }
 
 module.exports = { User, ClientUser };
