c5dbba62d8c5f313e315100d3b8e0b2b402f8434
Voltrex <62040526+VoltrexMaster@users.noreply.github.com>
2021-05-03 18:00:26 +0430

    Fix websocket module
    Fixed the `websocket` module by adding more details and convenient checks and applying correct identation.

--- websocket.js
+++ websocket.js
@@ -1,45 +1,47 @@
-const { Inflate }             = require('pako');
-const { camelCase, evaluate } = require('./Util');
-const Message                 = require('./constructors/Message');
-const { ClientUser }          = require('./constructors/User');
+const { Inflate } = require('pako');
+const { camelCase, evaluate } = require('./Util.js');
+const Message = require('./constructors/Message.js');
+const { ClientUser } = require('./constructors/User.js');
 
-module.exports = (bot, message, flag) => {
-    const msg = evaluate(message, flag);
-    bot.emit('raw', { event: (msg.t || 'UNKNOWN'), opCode: (msg.op || 0), data: (msg.d || {}) });
+module.exports = function websocket(bot, message, flag) {
+  const msg = evaluate(message, flag);
+  bot.emit('raw', {
+    event: msg.t || 'UNKNOWN',
+    opCode: msg.op || 0,
+    data: msg.d || {}
+  });
     
-    switch (msg.t) {
-        case 'READY':
-            if (!msg.d.user.bot) process.exit(1); // no selfbots allowed uwu
-            bot.sessionID = msg.d.session_id;
-            bot.user = new ClientUser(bot, msg.d.user);
-            bot.emit('ready');
-            bot.emit('debug', 'Successfully connected to the discord gateway. Gateway sends a READY event.');
-            break;
-        case 'MESSAGE_CREATE':
-            bot.emit('messageCreate', new Message(bot, msg.d));
-            break;
-        default:
-            if (msg.t && msg.d)
-                bot.emit(camelCase(msg.t), msg.d);
-    }
+  switch (msg.t) {
+    case 'READY':
+      if (!msg.d.user.bot) process.exit(1); // no selfbots allowed uwu
+      bot.sessionID = msg.d.session_id;
+      bot.user = new ClientUser(bot, msg.d.user);
+      bot.emit('ready');
+      bot.emit('debug', '[PAYLOAD: READY] Successfully connected to the Discord gateway.');
+      break;
+    case 'MESSAGE_CREATE':
+      bot.emit('messageCreate', new Message(bot, msg.d));
+      break;
+    default:
+      if (msg.t && msg.d)
+        bot.emit(camelCase(msg.t), msg.d);
+  }
     
-    switch (msg.op) {
-        case 7: // reconnect
-            return bot.socket.send(JSON.stringify({
-                op: 6,
-                d: {
-                    session_id: bot.sessionID,
-                    token: bot.token,
-                    seq: bot.seqNum
-                }
-            }));
-        case 10: // heartbeat
-            if (bot.hb) clearInterval(bot.hb);
-            bot.hb = setInterval(() => {
-                return bot.socket.send(JSON.stringify({
-                    op: 1,
-                    d: 1
-                }));
-            }, msg.d.heartbeat_interval);
-    }
-}
+  switch (msg.op) {
+    case 7: // reconnect
+      return bot.socket.send(JSON.stringify({
+        op: 6,
+        d: {
+          session_id: bot.sessionID,
+          token: bot.token,
+          seq: bot.seqNum
+        }
+      }));
+      break;
+    case 10: // heartbeat
+      if (bot.hb) clearInterval(bot.hb);
+      bot.hb = setInterval(() => {
+        return bot.socket.send(JSON.stringify({ op: 1, d: 1 }));
+      }, msg.d.heartbeat_interval);
+  }
+};
